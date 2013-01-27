7635606dac5f20a0f0b6ee8268a2788181f0a94b
hdenk <helmut.denk@gmx.de>
2013-01-27 12:28:06 +0100

    update buildfile

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject enlive-test "0.1.0-SNAPSHOT"
+  :description "Testing enlive"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [enlive/enlive "1.0.1"]
+                 [org.clojure/algo.monads "0.1.0"]])
